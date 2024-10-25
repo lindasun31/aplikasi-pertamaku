158063e947d8bdd5c91760e2b53a1b9805c5ec98
lindasun31 <129498464+lindasun31@users.noreply.github.com>
2024-10-26 01:25:23 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import sqlite3 from 'sqlite3';
 import cors from 'cors';
 
-const allowedOrigins = ['https://yourtrusteddomain.com'];
+const allowedOrigins = ['https://localhost:3000'];
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin)) {
