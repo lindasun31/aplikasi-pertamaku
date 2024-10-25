3d4c5a2bb453422c3e651b98eb57432b52c12124
lindasun31 <129498464+lindasun31@users.noreply.github.com>
2024-10-25 22:37:28 +0700

    Update CommentSection.vue

--- frontend/src/components/CommentSection.vue
+++ frontend/src/components/CommentSection.vue
@@ -2,19 +2,18 @@
 import { ref } from 'vue';
 
 const comment = ref('');
-const comments = ref('');
-
+const comments = ref([]);
 const submitComment = () => {
-  comments.value += `<p>${comment.value}</p>`;
+  comments.value.push({ id: Date.now(), text: comment.value });
   comment.value = '';
 };
-</script>
+
 
 <template>
   <div>
     <h3>Comments</h3>
     <input v-model="comment" placeholder="Leave a comment" />
     <button @click="submitComment">Submit</button>
-    <div v-html="comments"></div>
+    <div v-for="comment in comments" :key="comment.id">{{ comment.text }}</div>
   </div>
 </template>
